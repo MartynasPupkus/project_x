4c6fb6d19cb03d6189d69a010c93ecfaeb0ee66e
MartynasPupkus <pupkus.martynas@gmail.com>
2024-03-24 23:04:24 +0200

    sign up almost works

--- vinted/core/forms.py
+++ vinted/core/forms.py
@@ -3,7 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 
-class SignUpForm(UserCreationForm):
+class SignupForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password1','password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+    }))
+    
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email address',
+    }))
+    
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+    }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat password',
+    }))
--- vinted/core/views.py
+++ vinted/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from item.models import Category, Item
-from .forms import SignUpForm
+from .forms import SignupForm
 
 
 def index(request):
@@ -16,7 +16,15 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')
+    else:
+        form = SignupForm()
+    
+    form = SignupForm()
     return render(request, 'core/signup.html', {
         'form': form
     })
