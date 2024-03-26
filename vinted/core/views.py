5c1aef9530e7184c4843c1c06c9857d4bb1edcbf
MartynasPupkus <pupkus.martynas@gmail.com>
2024-03-26 22:47:41 +0200

    working bottom menu

--- vinted/core/views.py
+++ vinted/core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import logout
 
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:15]
+    items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
@@ -15,6 +15,15 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
+def privacy(request):
+    return render(request, 'core/privacy.html')
 
 def signup(request):
     if request.method == 'POST':
